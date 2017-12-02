5ca0232852709c63171a142d9043c2a7370ec2af
paul <4hoelzen@informatik.uni-hamburg.de>
2017-12-02 15:08:00 +0100

    ha07 - python kram bisschen weiter gemacht, geht aber noch nicht ganz

--- HA07/puzzle.py
+++ HA07/puzzle.py
@@ -34,22 +34,61 @@ def chk_constraints(d, word, pos):
     }
     return switch.get(pos, False)
 
-# domain consistency
-for pos in puzzle:
-    for word in puzzle[pos]:
-        if not chk_constraints(puzzle, word, pos):
-            puzzle[pos].remove(word)
-    print str(pos) + ": " + str(puzzle[pos])
-    print 
+def reduce_domains(d):
+    for pos in d:
+        for word in d[pos]:
+            if not chk_constraints(d, word, pos):
+                d[pos].remove(word)
+    return d
 
+def print_puzzle(p):
+    for i in p:
+        print str(i) + ": " + str(p[i])
+
+def solve_rec(p):
+    if not none_empty(p):
+        return False
+    #l = [y for x in p.values() for y in x]
+    if not [1 for x in p.values() if len(x) != 1]: # and len(l) == len(set(l)):
+        return p
+    else:
+        p1 = p2 = {}
+        for pos in p:
+            p1[pos] = p[pos][:len(p[pos]) / 2]
+            p2[pos] = p[pos][len(p[pos]) / 2:]
+        return solve_rec(p1) or solve_rec(p2)
+
+def vis_puzzle(p):
+    a = p["A1"][0][0]
+    b = p["A1"][0][1]
+    c = p["A1"][0][2]
+    d = p["A2"][0][0]
+    e = p["A2"][0][1]
+    f = p["A2"][0][2]
+    g = p["A3"][0][0]
+    h = p["A3"][0][1]
+    i = p["A3"][0][2]
+
+    print "   | D1 | D2 | D3"
+    print "---+----+----+---"
+    print "A1 | "+str(a)+"  | "+str(b)+"  | "+str(c)+" "
+    print "A2 | "+str(d)+"  | "+str(e)+"  | "+str(f)+" "
+    print "A3 | "+str(g)+"  | "+str(h)+"  | "+str(i)+" "
+
+print "Domain consistency"
+puzzle = reduce_domains(puzzle)
+print_puzzle(puzzle)
+print
+
+print "Arc consistency"
 diff = {}
 while not diff == puzzle:
-    # arc consistency
     diff = dict(puzzle)
-    for pos in puzzle.viewkeys():
-        for word in puzzle[pos]:
-            if not chk_constraints(puzzle, word, pos):
-                puzzle[pos].remove(word)
+    puzzle = reduce_domains(puzzle)
+print_puzzle(puzzle)
+print
 
-for k in puzzle:
-    print str(k) + str(puzzle[k])
+sol = solve_rec(puzzle)
+print sol
+print 
+vis_puzzle(sol)
